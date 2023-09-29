9d784451998113d9d2530cc17a0da4d652c086f9
Seanghay Yath <seanghay.dev@gmail.com>
2023-09-29 20:00:12 +0700

    feat: add index.d.ts

---
+++ src/index.d.ts
@@ -0,0 +1,30 @@
+export declare type MekongSMSOptions = {
+  endpoint: string;
+  username: string;
+  password: string;
+  sender?: string;
+}
+
+export declare type MekongSMSSendOptions = {
+  text: string;
+  sender: string;
+  phoneNumbers: string | string[];
+  international?: boolean;
+  customData?: string;
+}
+
+export declare type MekongSMSSendResult = {
+  code: number;
+  message: string;
+}
+
+export declare type MekongSMSCreditResult = {
+  message?: string;
+  credit?: number;
+}
+
+export declare class MekongSMS {
+  constructor(options: MekongSMSOptions);
+  async send(options: MekongSMSSendOptions): Promise<MekongSMSSendResult[]>;
+  async credits(): Promise<MekongSMSCreditResult>;
+}
